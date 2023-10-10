3c2065de1c8a85c9e3f7d33847f1ffea1234f90b
vimoreno21 <102815017+vimoreno21@users.noreply.github.com>
2023-10-10 13:20:49 -0400

    added random js

---
+++ src/components/random.js
@@ -0,0 +1,22 @@
+// Simple JavaScript code
+
+// Declare a variable and assign a value
+let message = "Hello, world!";
+
+// Print the message to the console
+console.log(message);
+
+// Create a function
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+// Call the function and print the result
+let greeting = greet("Alice");
+console.log(greeting);
+
+// Perform a basic operation
+let num1 = 5;
+let num2 = 3;
+let result = num1 + num2;
+console.log("The result of " + num1 + " + " + num2 + " is: " + result);
