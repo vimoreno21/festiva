33501d6c65095e787a6ce2388cbbbaab119d07b6
vimoreno21 <102815017+vimoreno21@users.noreply.github.com>
2023-10-12 11:48:17 -0400

    server.js working

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-const express = require('express');
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) =>
