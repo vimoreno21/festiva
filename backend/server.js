9c90e84644b6084edbbc7d3a4a3958174196df01
RicardoB1437 <ricardoblll@knights.ucf.edu>
2023-10-17 18:24:06 -0400

    made backend folder

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use((req, res, next) =>
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+
+if (process.env.NODE_ENV === 'production')
+{
+    // Set static folder
+    app.use(express.static('../frontend/build'));
+    app.get('*', (req, res) =>
+    {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
+
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.CONN_STRING;
+const client = new MongoClient(url);
+client.connect()
+
+app.listen(PORT, () =>
+{
+    console.log('Server listening on port ' + PORT);
+}); 
