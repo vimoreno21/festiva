46fffefeaca280717b2336f97bb58ed80c69cc3b
vimoreno21 <102815017+vimoreno21@users.noreply.github.com>
2023-10-16 23:54:25 -0400

    fixed overlay issue

--- frontend/src/components/LoginSignUp.js
+++ frontend/src/components/LoginSignUp.js
@@ -1,28 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginSignUp() {
-    const doLogin = async (event) => {
-        event.preventDefault();
-        alert('doIt()');
+    const [isSignUp, setIsSignUp] = useState(false);
+
+    const togglePanel = () => {
+        setIsSignUp(!isSignUp);
     };
 
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
-
-    if (signUpButton && signInButton && container) {
-        signUpButton.addEventListener('click', () => {
-            container.classList.add('right-panel-active');
-        });
-
-        signInButton.addEventListener('click', () => {
-            container.classList.remove('right-panel-active');
-        });
-    }
+    const doLogin = async (event) => {
+        event.preventDefault();
+        alert('doIt()'); // Replace this with your desired login logic
+    };
 
     return (
-        <div>
-            <div className="container" id="container">
+        <div> 
+            <div className={`container ${isSignUp ? 'right-panel-active' : ''}`} id="container">
                 <div className="form-container sign-up-container">
                     <form onSubmit={doLogin}>
                         <h1>Create Account</h1>
@@ -37,25 +29,26 @@ function LoginSignUp() {
                         <h1>Sign in</h1>
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
-                        <a href={doLogin} onClick={doLogin}>Forgot your password?</a>
+                        <p onClick={togglePanel}>Forgot your password?</p>
                         <button type="submit">Sign In</button>
                     </form>
                 </div>
                 <div className="overlay-container">
                     <div className="overlay">
-                        <div className="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>To keep connected with us please login with your personal info</p>
-                            <button className="ghost" id="signIn">
-                                Sign In
-                            </button>
-                        </div>
-                        <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>Enter your personal details and start the journey with us</p>
-                            <button className="ghost" id="signUp">
-                                Sign Up
-                            </button>
+                        <div className={`overlay-panel ${isSignUp ? 'overlay-left' : 'overlay-right'}`}>
+                            {isSignUp ? (
+                                <>
+                                    <h1>Hello, Friend!</h1>
+                                    <p>Enter your personal details and start the journey with us</p>
+                                    <button className="ghost" onClick={togglePanel}id="signUp">Sign Up</button>
+                                </>
+                            ) : (
+                                <>
+                                    <h1>Welcome Back!</h1>
+                                    <p>To keep connected with us please login with your personal info</p>
+                                    <button className="ghost" onClick={togglePanel} id="signIn">Sign In</button>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
