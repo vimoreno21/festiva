6be8422097b164dc2e4c601e83ec190c49b5bd99
vimoreno21 <102815017+vimoreno21@users.noreply.github.com>
2023-10-19 00:11:53 -0400

    removed manifest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
+// import React from 'react';
+// import LoginPage from './pages/LoginPage';
+// import StartPage from './pages/StartPage';
+// function App()
+// {
+//   return (
+//     <StartPage />
+//   );
+// }
+// export default App;
+
 import React from 'react';
-import LoginPage from './pages/LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
-function App()
-{
+import LoginPage from './pages/LoginPage';
+
+function App() {
   return (
-    <StartPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
