48a970fed5623bcd95d6d32e8ac093e219f59532
melanieviolett <melanieviolettt@gmail.com>
2023-10-18 20:17:39 -0400

    debugging

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
+console.log("1");
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+console.log("2");
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
+console.log("3");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -24,6 +28,7 @@ app.use((req, res, next) =>
     );
     next();
 });
+console.log("4");
 
 
 if (process.env.NODE_ENV === 'production')
@@ -36,22 +41,27 @@ if (process.env.NODE_ENV === 'production')
     });
 }
 
+console.log("5");
 
 const MongoClient = require('mongodb').MongoClient;
 const url = process.env.CONN_STRING;
 const client = new MongoClient(url);
 client.connect()
+console.log("6");
 
 app.listen(PORT, () =>
 {
     console.log('Server listening on port ' + PORT);
 }); 
+console.log("7");
 
 app.post('/api/login', async (req, res, next) => 
 {
   // incoming: login, password
   // outgoing: id, firstName, lastName, error
 	
+    console.log("in login endpoint");
+
  var error = '';
 
   const { email, password } = req.body;
@@ -59,6 +69,7 @@ app.post('/api/login', async (req, res, next) =>
   const db = client.db();
   const results = await db.collection('users').find({email:email,password:password}).toArray();
 
+  console.log("8");
 
   var id = -1;
   var name = '';
@@ -70,8 +81,11 @@ app.post('/api/login', async (req, res, next) =>
     name = results[0].name;
     avatar = results[0].avatar;
   }
+  console.log("9");
 
   var ret = { id:id, name:name, avatar:avatar, error:''};
   console.log(ret)
   res.status(200).json(ret);
+  console.log("10");
+
 });
