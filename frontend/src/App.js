f70fcfd6f771a37af454ab6774211d01d76bef70
melanieviolett <melanieviolettt@gmail.com>
2023-10-21 22:41:54 -0400

    fixed routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<StartPage/>}/>
       </Routes>
     </BrowserRouter>
   );
