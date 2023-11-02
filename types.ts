e87696a14d13b5282e2e60336a3f3abaffb288f6
truearian <139094765+truearian@users.noreply.github.com>
2023-11-02 19:09:09 -0400

    Delete types.ts

--- types.ts
+++
@@ -1,8 +0,0 @@
-export type RootStackParamList = {
-    Home: undefined;
-    Login: undefined;
-    Signup: undefined;
-    Main: undefined;
-    Guest: undefined;
-    // Add other screen names and their param types as needed
-  };
