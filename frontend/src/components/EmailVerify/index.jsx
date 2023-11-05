a86ac5c6e3d069320116893d7ddf0c21f235d00d
RicardoB1437 <ricardoblll@knights.ucf.edu>
2023-11-05 15:41:58 -0500

    adding rest of files

---
+++ frontend/src/components/EmailVerify/index.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import styles from "./styles.module.css";
+import { Fragment } from 'react';
+
+const EmailVerify = () => {
+	const [validUrl, setValidUrl] = useState(true);
+	const param = useParams();
+
+	useEffect(() => {
+		const verifyEmailUrl = async () => {
+			try {
+				const url = `http://localhost:5000/api/users/${param.id}/verify/${param.token}`;
+				const { data } = await axios.get(url);
+				console.log(data);
+				setValidUrl(true);
+			} catch (error) {
+				console.log(error);
+				setValidUrl(false);
+			}
+		};
+		verifyEmailUrl();
+	}, [param]);
+
+	return (
+		<Fragment>
+			{validUrl ? (
+				<div className={styles.container}>
+					<h1>Email verified successfully</h1>
+					<Link to="/login">
+						<button className={styles.green_btn}>Login</button>
+					</Link>
+				</div>
+			) : (
+				<h1>404 Not Found</h1>
+			)}
+		</Fragment>
+	);
+};
+
+export default EmailVerify;
