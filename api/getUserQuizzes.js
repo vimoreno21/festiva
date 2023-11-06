6850a003544a488bf64b99492001256f4d02aef2
RicardoB1437 <ricardoblll@knights.ucf.edu>
2023-11-05 21:50:52 -0500

    all quiz apis done

--- api/getUserQuizzes.js
+++ api/getUserQuizzes.js
@@ -1,22 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Quiz = require('../models/quizModel')
+const Quiz = require("../models/quizModel");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  const id = req.body._id;
 
-    const id = req.body._id
+  results = await Quiz.find({
+    owner_id: id,
+    quiz_name: { $regex: "", $options: "i" },
+  });
 
-    results = await Quiz.find({owner_id: id, quiz_name: {$regex: "", $options: "i"}})
+  if (results.length > 0) {
+    return res.status(200).send(results);
+  } else {
+    return res.status(400).send({ message: "No quizzes found." });
+  }
+});
 
-    if (results.length > 0)
-    {
-        return res.status(200).send(results)
-    }
-    else
-    {
-        return res.status(400).send({message: "No quizzes found."})
-    }
-
-})
-
-module.exports = router ;
+module.exports = router;
--- api/searchUserQuiz.js
+++ api/searchUserQuiz.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Quiz = require('../models/quizModel')
+const Quiz = require("../models/quizModel");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  const name = req.body.quiz_name;
+  const id = req.body._id;
 
-    const name = req.body.quiz_name;
-    const id = req.body._id
+  results = await Quiz.find({
+    owner_id: id,
+    quiz_name: { $regex: name, $options: "i" },
+  });
 
-    results = await Quiz.find({owner_id: id, quiz_name: {$regex: name, $options: "i"}})
+  if (results.length > 0) {
+    return res.status(200).send(results);
+  } else {
+    return res.status(400).send({ message: "No quizzes found." });
+  }
+});
 
-    if (results.length > 0)
-    {
-        return res.status(200).send(results)
-    }
-    else
-    {
-        return res.status(400).send({message: "No quizzes found."})
-    }
-
-})
-
-module.exports = router ;
+module.exports = router;
