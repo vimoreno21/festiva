2f79a4ce0bffae58c30f6ffd1271b98d63ce4cf8
RicardoB1437 <ricardoblll@knights.ucf.edu>
2023-11-06 21:22:04 -0500

    changing verification link again

--- api/registerVerification.js
+++ api/registerVerification.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/userModel')
 
-router.get("/:id/verify/:token", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
         console.log("inside register verification!")
         console.log(req.params);
