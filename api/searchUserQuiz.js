8bcd0fb6504f4b18ed3a22614b702be3ec75ea17
RicardoB1437 <ricardoblll@knights.ucf.edu>
2023-11-07 14:37:59 -0500

    adding in user id to userQuiz api

--- api/searchUserQuiz.js
+++ api/searchUserQuiz.js
@@ -5,9 +5,9 @@ const Quiz = require('../models/quizModel')
 router.post('/', async (req, res) => {
 
     const name = req.body.quiz_name;
-    console.log(name)
+    const id = req.body._id
 
-    results = await Quiz.find({quiz_name: {$regex: name, $options: "i"}})
+    results = await Quiz.find({_id: id, quiz_name: {$regex: name, $options: "i"}})
 
     if (results.length > 0)
     {
