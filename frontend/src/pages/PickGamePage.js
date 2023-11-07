2e046355873865ac6bf0efe51f7a6499dbdc41be
muhadeseh <muhadeseha@gmail.com>
2023-11-07 12:30:15 -0500

    changed description for quizoot game

--- frontend/src/pages/PickGamePage.js
+++ frontend/src/pages/PickGamePage.js
@@ -1,60 +1,75 @@
-import React from 'react'
+import React from "react";
 import { GiPartyPopper } from "react-icons/gi";
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 
 // not responsive yet
 const PickGamePage = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const [game, setGame] = useState("");
-const onClickGame = () => {
+  const [game, setGame] = useState("");
+  const onClickGame = () => {
     setGame(game);
-}
+  };
 
-const onPlayGame = () => {
+  const onPlayGame = () => {
     if (game) {
-        if (game === "Fill in the Blank") {
-            navigate('/waitToPlayGame')
-        }
+      if (game === "Fill in the Blank") {
+        navigate("/waitToPlayGame");
+      }
     }
-}
+  };
 
   return (
     <>
-    <div className='pick_game_style container-p'>
-        
-        <div className='flex flex-col md:flex-row justify-around'>
-            <div className='md:w-50 div_style flex flex-col'>
-                
-                <p className='fs-2 fw-bold'> <GiPartyPopper/> Step 1: Pick A Game <GiPartyPopper/></p>
-                <div className='border border-dark p-4'>
-                    <p className='fw-normal fs-5'>    
-                        Get ready for an electrifying game of creative showdown! In this rapid-fire competition, you'll be randomly paired against two other players, and all of you will have a limited time to submit your responses to two prompts. 
-                    </p>
+      <div className="pick_game_style container-p">
+        <div className="flex flex-col md:flex-row justify-around">
+          <div className="md:w-50 div_style flex flex-col">
+            <p className="fs-2 fw-bold">
+              {" "}
+              <GiPartyPopper /> Step 1: Pick A Game <GiPartyPopper />
+            </p>
+            <div className="border border-dark p-4">
+              <p className="fw-normal fs-5">
+                Get ready to quiz your way to a good time! Our game is all about
+                the joy of quizzing, whether you're chilling with friends on a
+                weekend, spicing up a family gathering, or even impressing your
+                coworkers during a team-building session.
+              </p>
 
-                    <p className='fw-normal fs-5'>
-                        It's a test of your quick thinking, humor, and creativity. Once all responses are in, it's time for a thrilling voting round where you'll decide which response reigns supreme, prompt by prompt. 
-                    </p>
-                    <p className='fw-bold fs-5'>
-                        Can you outshine your two rivals and become the ultimate champion of wit and wordplay? 
-                    </p>
-                </div>
-
+              <p className="fw-normal fs-5">
+                Who knows, you might just discover that the quietest person in
+                the room is a quizmaster in disguise! It's the ultimate
+                ice-breaker for parties, a great way to settle friendly debates.
+              </p>
+              <p className="fw-bold fs-5">
+                Get ready to for some quiz showdowns!
+              </p>
             </div>
-            <div className='md:w-50 div_style flex flex-col'>
-                <div className='w-75 h-100 set_back fs-3 rounded text-center justify-content-center flex'>
-                    <button value={game} onClick={onClickGame} className='button_style border h-25 border-light set_color text-nowrap fs-5'>Quizoot</button>
-                </div>
-                <Link to='/quizGameLibrary' onClick={onPlayGame} className='button_style2 w-50 text-decoration-none'>Play Now</Link>
+          </div>
+          <div className="md:w-50 div_style flex flex-col">
+            <div className="w-75 h-100 set_back fs-3 rounded text-center justify-content-center flex">
+              <button
+                value={game}
+                onClick={onClickGame}
+                className="button_style border h-25 border-light set_color text-nowrap fs-5"
+              >
+                Quizoot
+              </button>
             </div>
-
+            <Link
+              to="/quizGameLibrar"
+              onClick={onPlayGame}
+              className="button_style2 w-50 text-decoration-none"
+            >
+              Play Now
+            </Link>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default PickGamePage
+export default PickGamePage;
