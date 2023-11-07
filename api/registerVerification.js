eecc8e44b8037169408407873f480bd0b0a81a35
RicardoB1437 <ricardoblll@knights.ucf.edu>
2023-11-06 21:33:10 -0500

    changing verification to post

--- api/registerVerification.js
+++ api/registerVerification.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/userModel')
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
         console.log("inside register verification!")
         console.log(req.params);
