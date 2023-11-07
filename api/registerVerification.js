be03d5ae893671f24dd19824b4dc71531ff75d85
RicardoB1437 <ricardoblll@knights.ucf.edu>
2023-11-06 21:58:20 -0500

    rolling back some register changes

--- api/registerVerification.js
+++ api/registerVerification.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/userModel')
 
-router.post("/", async (req, res) => {
+router.post("/:id/verify/:token", async (req, res) => {
     console.log("inside register verification!")
     try {
         console.log("inside register verification!")
