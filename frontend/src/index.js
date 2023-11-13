234590a18482dc21196267a5debcac2bec3c82de
melanieviolett <melanieviolettt@gmail.com>
2023-11-13 18:37:39 -0500

    added delete endpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {NextUIProvider} from "@nextui-org/react";
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NextUIProvider>
     <App />
-    </NextUIProvider>
-     
-      
   </React.StrictMode>
 );
 
